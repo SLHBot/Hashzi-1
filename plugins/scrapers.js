5070ff075fd9ca8de60ec0d491c7f6f827439cf8
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-08-18 01:40:10 +0530

    new

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -504,7 +504,7 @@ if (config.WORKTYPE == 'private') {
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid, Lang.UPLOADING_VIDEO, MessageType.text);
-            await message.client.sendMessage(message.jid, fs.readFileSync('./' + VID + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4 });
+            await message.client.sendMessage(message.jid, fs.readFileSync('./' + VID + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4 }, { caption: "Copyright © 2021 | SLHackers Team Bot " });
         });
     }));
 
@@ -972,7 +972,7 @@ else if (config.WORKTYPE == 'public') {
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid, Lang.UPLOADING_VIDEO, MessageType.text);
-            await message.client.sendMessage(message.jid, fs.readFileSync('./' + VID + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4 });
+            await message.client.sendMessage(message.jid, fs.readFileSync('./' + VID + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4 }, { caption: "Copyright © 2021 | SLHackers Team Bot " });
         });
     }));
 
