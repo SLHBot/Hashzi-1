a62d15c82079504ba5690707f75658fa0466e068
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-08-19 02:15:23 +0530

    phub add

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -20,9 +20,9 @@ if (Config.WORKTYPE == 'private') {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
-        var webimage = await axios.get(`https://shot.screenshotapi.net/screenshot?&url=${match[1]}&width=1920&height=1080&full_page=true&fresh=true&output=image&file_type=png&block_ads=true&no_cookie_banners=true&lazy_load=true&dark_mode=true&block_tracking=true&wait_for_event=load`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://shot.screenshotapi.net/screenshot?&url=${match[1]}&width=1920&height=1080&full_page=true&extract_text=true&output=image&file_type=pdf&pdf_options%5Bformat%5D)=A4&pdf_options%5Bmedia%5D)=print&pdf_options%5Blandscape%5D)=false&pdf_options%5Bprint_background%5D)=false&block_ads=true&no_cookie_banners=true&lazy_load=true&dark_mode=true&block_tracking=true&wait_for_event=load`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by *🔰𝒮𝐿𝐻𝒶𝒸𝓀𝑒𝓇𝓈 𝒯𝑒𝒶𝓂 𝐵❁𝓉🔰*' })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by *⚙ SLHackers Bot ⚙*' })
 
     }));
 }
@@ -32,18 +32,18 @@ else if (Config.WORKTYPE == 'public') {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
-        var webimage = await axios.get(`https://shot.screenshotapi.net/screenshot?&url=${match[1]}&width=1920&height=1080&full_page=true&fresh=true&output=image&file_type=png&block_ads=true&no_cookie_banners=true&lazy_load=true&dark_mode=true&block_tracking=true&wait_for_event=load`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://shot.screenshotapi.net/screenshot?&url=${match[1]}&width=1920&height=1080&full_page=true&extract_text=true&output=image&file_type=pdf&pdf_options%5Bformat%5D)=A4&pdf_options%5Bmedia%5D)=print&pdf_options%5Blandscape%5D)=false&pdf_options%5Bprint_background%5D)=false&block_ads=true&no_cookie_banners=true&lazy_load=true&dark_mode=true&block_tracking=true&wait_for_event=load`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by *🔰𝒮𝐿𝐻𝒶𝒸𝓀𝑒𝓇𝓈 𝒯𝑒𝒶𝓂 𝐵❁𝓉🔰*' })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by *⚙ SLHackers Bot ⚙*' })
 
     }));
     Asena.addCommand({ pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC, dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
-        var webimage = await axios.get(`https://shot.screenshotapi.net/screenshot?&url=${match[1]}&width=1920&height=1080&full_page=true&fresh=true&output=image&file_type=png&block_ads=true&no_cookie_banners=true&lazy_load=true&dark_mode=true&block_tracking=true&wait_for_event=load`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://shot.screenshotapi.net/screenshot?&url=${match[1]}&width=1920&height=1080&full_page=true&extract_text=true&output=image&file_type=pdf&pdf_options%5Bformat%5D)=A4&pdf_options%5Bmedia%5D)=print&pdf_options%5Blandscape%5D)=false&pdf_options%5Bprint_background%5D)=false&block_ads=true&no_cookie_banners=true&lazy_load=true&dark_mode=true&block_tracking=true&wait_for_event=load`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by *🔰𝒮𝐿𝐻𝒶𝒸𝓀𝑒𝓇𝓈 𝒯𝑒𝒶𝓂 𝐵❁𝓉🔰*' })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by *⚙ SLHackers Bot ⚙*' })
 
     }));
 }
