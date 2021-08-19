03f944952915de267936914263803c3dc0fd400d
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-08-20 03:11:22 +0530

    🆆🅾🆁🅺 🅷🅰🆁🅳

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ async function whatsAsena() {
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('LK', { timeZone: 'Asia/Colombo' }).split(' ')[1]
-                const biography = 'අවසන් වරට මම Online සිටි වේලාව.\n\n' + '📅 ' + utch + '\n⌚ ' + ov_time + '\n\n 🎲𝑺̲̅𝒂̲̅𝒏̲̅𝒅̲̅𝒖̲̅𝒔̲̅𝒉̲̅𝒊̲̅ 𝑳̲̅𝒐̲̅𝒉̲̅𝒊̲̅𝒏̲̅𝒊̲̅𝒌̲̅𝒂̲̅🎲'
+                const biography = '🆆🅾🆁🅺 🅷🅰🆁🅳 අවසන් වරට මම Online සිටි වේලාව.\n\n' + '📅 ' + utch + '\n⌚ ' + ov_time + '\n\n🎲𝑺̲̅𝒂̲̅𝒏̲̅𝒅̲̅𝒖̲̅𝒔̲̅𝒉̲̅𝒊̲̅ 𝑳̲̅𝒐̲̅𝒉̲̅𝒊̲̅𝒏̲̅𝒊̲̅𝒌̲̅𝒂̲̅🎲'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('351')) { // Portugal
