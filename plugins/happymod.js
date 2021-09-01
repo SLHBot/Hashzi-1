bacb959ee93f1fb929aa9dd01d6fea7ef092d76d
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-01 18:05:45 +0530

    happymod update from eva

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -28,14 +28,16 @@ else if (Config.WORKTYPE == 'public') {
 
 	Asena.addCommand({ pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 		if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-		const url = `https://api.zeks.me/api/happymod?apikey=qzXRlER89xXLoOvXH8VPmVCh5ye&q=${match[1]}`;
+		const url = `https://api.zeks.xyz/api/happymod?apikey=SUcKEWdDpYjtj2q045KNQCoDyCe&q=${match[1]}`;
 		try {
 			const response = await got(url);
 			const json = JSON.parse(response.body);
 			if (response.statusCode === 200) return await message.client.sendMessage(message.jid,
-				'*📕 ' + Lang.NAMEY + '* ```' + json.result[0].title + '```\n\n' +
+				'*📘 ' + Lang.IMAGE + '* ```' + json.result[0].thumb + '```\n\n\n' +
+				'*📕 ' + Lang.NAME + '* ```' + json.result[0].title + '```\n\n' +
+				'*📘 ' + Lang.RATING + '* ```' + json.result[0].rating + '```\n\n\n' +
 				'*📘 ' + Lang.SIZE + '* ```' + json.result[0].size + '```\n\n\n' +
-				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].link + '```\n', MessageType.text);
+				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].url + '```\n', MessageType.text);
 		} catch {
 			return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 		}
@@ -43,14 +45,16 @@ else if (Config.WORKTYPE == 'public') {
 
 	Asena.addCommand({ pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 		if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-		const url = `https://api.zeks.me/api/happymod?apikey=qzXRlER89xXLoOvXH8VPmVCh5ye&q=${match[1]}`;
+		const url = `https://api.zeks.xyz/api/happymod?apikey=SUcKEWdDpYjtj2q045KNQCoDyCe&q=${match[1]}`;
 		try {
 			const response = await got(url);
 			const json = JSON.parse(response.body);
 			if (response.statusCode === 200) return await message.client.sendMessage(message.jid,
-				'*📕 ' + Lang.NAMEY + '* ```' + json.result[0].title + '```\n\n' +
+				'*📘 ' + Lang.IMAGE + '* ```' + json.result[0].thumb + '```\n\n\n' +
+				'*📕 ' + Lang.NAME + '* ```' + json.result[0].title + '```\n\n' +
+				'*📘 ' + Lang.RATING + '* ```' + json.result[0].rating + '```\n\n\n' +
 				'*📘 ' + Lang.SIZE + '* ```' + json.result[0].size + '```\n\n\n' +
-				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].link + '```\n', MessageType.text);
+				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].url + '```\n', MessageType.text);
 		} catch {
 			return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 		}
