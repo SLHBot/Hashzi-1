e18b4f78c80f1a68261b3bbdddc9ae9ef2aec2ff
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-02 17:08:33 +0530

    random error fix

--- plugins/random.js
+++ plugins/random.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const NEED_WORD = "*Must Enter some Words*"
 
 if (Config.WORKTYPE == 'private') {
-    Asena.addCommand({ pattern: 'random ?(.*)', fromMe: false, desc: 'word image' }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'random ?(.*)', fromMe: true, desc: 'word image' }, (async (message, match) => {
 
         var r_text = new Array();
 
