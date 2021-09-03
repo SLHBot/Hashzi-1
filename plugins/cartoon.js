329ca7e6d040c15959bf1bc351080283457a04c8
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-03 21:43:53 +0530

    only aded barcode/bgm/cartoon/gsmarena/linkvar

---
+++ plugins/cartoon.js
@@ -0,0 +1,39 @@
+
+/* Copyright (C) 2021 HIRUSHA.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsenaDuplicated*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "*type some word after command*\n*command enthelum perupara nenba"
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'cartoon(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/ephoto1/cartoongravity?apikey=03ffdf45513a71ed7e669db1&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by SLHackers' })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'cartoon(.*)', fromMe: false, dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/ephoto1/cartoongravity?apikey=03ffdf45513a71ed7e669db1&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by SLHackers' })
+
+    }));
+
+}
