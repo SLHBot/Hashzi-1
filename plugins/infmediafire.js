e1b3a98382bf839867589beb257509c936f0f8bd
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-03 21:45:13 +0530

    Only aded & edited infmediafire/oldphoto/randomname

---
+++ plugins/infmediafire.js
@@ -0,0 +1,49 @@
+/* created by HIRUSHA
+*/
+
+const Asena = require('../events');
+const { MessageType } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+
+
+if (Config.WORKTYPE == 'private') {
+
+	Asena.addCommand({ pattern: 'infmediafire ?(.*)', fromMe: true, desc: Lang.DEVICE, dontAddCommandList: false }, async (message, match) => {
+		const url = `https://docs-jojo.herokuapp.com/api/mediafire?url=${match[1]}`;
+		try {
+			const response = await got(url);
+
+			const json = JSON.parse(response.body);
+
+			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 'FILE NAME      : *' + json.filename + '* \n' + 'DOWNLOAD URL      :' + json.url + 'SIZE    :' + json.filesize + '\n' + 'UPLOAD DATE & TIME   :' + json.uploaded + '\n' + 'FILE TYPE       :' + json.filetype + '\n' + 'SLHACKERS TEAM HACKED DETAILS   :' + json.desc, MessageType.text);
+		} catch {
+			return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+		}
+	});
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+	Asena.addCommand({ pattern: 'infmediafire ?(.*)', fromMe: false, desc: Lang.DEVICE, dontAddCommandList: false }, async (message, match) => {
+		const url = `https://docs-jojo.herokuapp.com/api/mediafire?url=${match[1]}`;
+		try {
+			const response = await got(url);
+
+			const json = JSON.parse(response.body);
+
+			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '``` FILE NAME      : *' + json.filename + '* \n' + 'DOWNLOAD URL      :' + json.url + 'SIZE    :' + json.filesize + '\n' + 'UPLOAD DATE & TIME   :' + json.uploaded + '\n' + 'FILE TYPE       :' + json.filetype + '\n' + 'SLHACKERS TEAM HACKED DETAILS   :' + json.desc + '```', MessageType.text);
+		} catch {
+			return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+		}
+	});
+	Asena.addCommand({ pattern: 'what now', fromMe: true, desc: Lang.NEWEP }, (async (message, match) => {
+
+		await message.sendMessage('*CMND* \n .device\n *DESC* \n know details of a mobilephone\n *example:* .device rog 5\n\n *CMND* \n .rdmore\n *DESC* \n add readmore after give text\n *example:* .rdmore SLHackers\n\n *CMND* \n.brdmore\n *DESC* \n add readmore before given text\n *example:* .brdmore SLHackers\n\n *CMND* \n.bgm one \n .bgm two \n *DESC* \n will change the type of reply bgm \n *example:* .bgm one (.bgm one \n is the default bgm mode - to change that use .bgm two ) ', MessageType.text, { quoted: message.data });
+
+	}));
+}
