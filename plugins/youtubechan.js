1c94c667f81e78e1bc59149515080a61d02fe362
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-06 23:18:34 +0530

    Only change youtubechan for sfb

--- plugins/youtubechan.js
+++ plugins/youtubechan.js
@@ -1,27 +1,49 @@
-/* Copyright (C) 2020 TOXIC DEVIL
-CODDED BY TOXIC DEVIL
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsAsenaPublic - TEENUH
+/* created by HIRUSHA
 */
 
 const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const axios = require('axios');
-const request = require('request');
-const got = require("got");
+const { MessageType } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
 
-// Sentances
-const QR_DESC = "It Converts Text to mobile legends Logo"
-const NEED_TEXT = "*🪐Must Enter Some Words*"
+const Language = require('../language');
+const Lang = Language.getString('weather');
 
-Asena.addCommand({ pattern: 'ytchannel ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid, NEED_WORD);
 
-    var webimage = await axios.get(`http://docs-jojo.herokuapp.com/api/gaming?text=${match[1]}`, { responseType: 'arraybuffer' })
+if (Config.WORKTYPE == 'private') {
 
-    await message.client.sendMessage(message.jid, Buffer.from(webimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Produced by*:\n⚡𝑺̲̅𝑳̲̅𝑯̲̅𝒂̲̅𝒄̲̅𝒌̲̅𝒆̲̅𝒓̲̅𝒔̲̅ 𝑻̲̅𝒆̲̅𝒂̲̅𝒎̲̅ 𝑩̲̅𝒐̲̅𝒕̲̅⚡' })
+    Asena.addCommand({ pattern: 'sfb ?(.*)', fromMe: true, desc: Lang.DEVICE, dontAddCommandList: false }, async (message, match) => {
+        const url = `https://api.lolhuman.xyz/api/facebook2?apikey=${Config.LLHAPI}&url=${match[1]}`;
+        try {
+            const response = await got(url);
 
-}));
+            const json = JSON.parse(response.body);
+
+            if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 'Result      : *' + json.message + '* \n\n' + 'DOWNLOAD URL      : *' + json.result + '*', MessageType.text);
+        } catch {
+            return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+        }
+    });
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'sfb ?(.*)', fromMe: false, desc: Lang.DEVICE, dontAddCommandList: false }, async (message, match) => {
+        const url = `https://api.lolhuman.xyz/api/facebook2?apikey=${Config.LLHAPI}&url=${match[1]}`;
+        try {
+            const response = await got(url);
+
+            const json = JSON.parse(response.body);
+
+            if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 'Result      : *' + json.message + '* \n\n' + 'DOWNLOAD URL      : *' + json.result + '*', MessageType.text);
+        } catch {
+            return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+        }
+    });
+    Asena.addCommand({ pattern: 'what now', fromMe: true, desc: Lang.NEWEP }, (async (message, match) => {
+
+        await message.sendMessage('*CMND* \n .device\n *DESC* \n know details of a mobilephone\n *example:* .device rog 5\n\n *CMND* \n .rdmore\n *DESC* \n add readmore after give text\n *example:* .rdmore SLHackers\n\n *CMND* \n.brdmore\n *DESC* \n add readmore before given text\n *example:* .brdmore SLHackers\n\n *CMND* \n.bgm one \n .bgm two \n *DESC* \n will change the type of reply bgm \n *example:* .bgm one (.bgm one \n is the default bgm mode - to change that use .bgm two ) ', MessageType.text, { quoted: message.data });
+
+    }));
+}
