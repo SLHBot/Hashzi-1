752bf5621aa0fd94dd0a81f42a54ea50a8c7d34b
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-07 00:18:46 +0530

    Only added fbvideo.js

---
+++ plugins/fbvideo.js
@@ -0,0 +1,38 @@
+/* created by HIRUSHA
+*/
+
+const Asena = require('../events');
+const { MessageType } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'vfb ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid, NEED_WORD);
+
+        var webimage = await axios.get(`${match[1]}`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid, Buffer.from(webimage.data), MessageType.video, { mimetype: Mimetype.mp4 })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'vfb ?(.*)', fromMe: false, dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid, NEED_WORD);
+
+        var webimage = await axios.get(`${match[1]}`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid, Buffer.from(webimage.data), MessageType.video, { mimetype: Mimetype.mp4 })
+
+    }));
+}
