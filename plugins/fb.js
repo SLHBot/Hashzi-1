2c837f324baa345dc5b1f0d6bcdb938274a07646
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-09 03:25:21 +0530

    Only add sfb

--- plugins/fb.js
+++ plugins/fb.js
@@ -19,18 +19,18 @@ const NO_RESULT = "*🌀can't Find Anything...*"
 
 if (Config.WORKTYPE == 'private') {
 
-    SlHackers.addCommand({ pattern: 'youtube ?(.*)', fromMe: true, desc: YTV_DESC, deleteCommand: false }, async (message, match) => {
+    SlHackers.addCommand({ pattern: 'slfb ?(.*)', fromMe: true, desc: YTV_DESC, deleteCommand: false }, async (message, match) => {
 
         const link = match[1]
 
         if (!link) return await message.client.sendMessage(message.jid, YT_NEED, MessageType.text)
         await message.client.sendMessage(message.jid, DWLOAD_VID, MessageType.text);
         await axios
-            .get(`https://api.lolhuman.xyz/api/ytvideo?apikey=${Config.LLHAPI}&url=${link}`)
+            .get(`https://api.lolhuman.xyz/api/facebook2?apikey=${Config.LLHAPI}&url=${link}`)
             .then(async (response) => {
                 const {
                     link,
-                } = response.data.result.link
+                } = response.data.result
 
                 const videoBuffer = await axios.get(link, { responseType: 'arraybuffer' })
 
@@ -45,18 +45,18 @@ if (Config.WORKTYPE == 'private') {
 }
 
 else if (Config.WORKTYPE == 'public') {
-    SlHackers.addCommand({ pattern: 'youtube ?(.*)', fromMe: false, desc: YTV_DESC }, async (message, match) => {
+    SlHackers.addCommand({ pattern: 'slfb ?(.*)', fromMe: false, desc: YTV_DESC }, async (message, match) => {
 
         const link = match[1]
 
         if (!link) return await message.client.sendMessage(message.jid, YT_NEED, MessageType.text, { quoted: message.data })
         await message.client.sendMessage(message.jid, DWLOAD_VID, MessageType.text)
         await axios
-            .get(`https://api.lolhuman.xyz/api/ytvideo?apikey=${Config.LLHAPI}&url=${link}`)
+            .get(`https://api.lolhuman.xyz/api/facebook2?apikey=${Config.LLHAPI}&url=${link}`)
             .then(async (response) => {
                 const {
                     link,
-                } = response.data.result.link
+                } = response.data.result
 
                 const videoBuffer = await axios.get(link, { responseType: 'arraybuffer' })
 
