fe48354861546ab8cf71195fd85dd9b4942de55e
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-08 19:05:06 +0530

    new update link detect

--- plugins/linkdetect.js
+++ plugins/linkdetect.js
@@ -15,39 +15,30 @@ const XN_N = "```වීඩියෝව 𝔻𝕠𝕨𝕟𝕝𝕠𝕒𝕕𝕚𝕟�
 const XN_D = "```📥වීඩියෝව 𝕌𝕡𝕝𝕠𝕒𝕕𝕚𝕟𝕘...```"
 const NEED_WORD = "*ඔබ 📥වීඩියෝවක URL ඇතුළත් කළ යුතුය*"
 
-var antilink_var = 'true'
 
-var ldc = ''
-if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
-if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
-if (Config.LANG == 'EN') ldc = '*Link Finded!*'
-if (Config.LANG == 'ML') ldc = '*ലിങ്ക് കണ്ടെത്തി!*'
-if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
-if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
-if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
-if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
-if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
 Asena.addCommand({ on: 'text', fromMe: false, deleteCommand: false }, (async (message, match) => {
+    var antilink_var = 'true'
+
+    var ldc = ''
+    if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
+    if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
+    if (Config.LANG == 'EN') ldc = '*Link Found!*'
+    if (Config.LANG == 'ML') ldc = '*ലിങ്ക് കണ്ടെത്തി!*'
+    if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
+    if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
+    if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
+    if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
+    if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
     if (antilink_var == 'true' && message.jid !== '905511384572-1616356915@g.us') {
         let regex1 = new RegExp('http://')
         let regex2 = new RegExp('https://')
         if (regex1.test(message.message)) {
-            var im = await checkImAdmin(message)
-            if (!im) return;
             await message.client.sendMessage(message.jid, ldc, MessageType.text, { quoted: message.data })
         }
         else if (regex2.test(message.message)) {
-            var us = await checkUsAdmin(message)
-            var im = await checkImAdmin(message)
-            if (!im) return;
-            if (us) return;
             await message.client.sendMessage(message.jid, ldc, MessageType.text, { quoted: message.data })
         }
         else if (message.message.match(/((?:[.]com)\b)/i)) {
-            var us = await checkUsAdmin(message)
-            var im = await checkImAdmin(message)
-            if (!im) return;
-            if (us) return;
             await message.client.sendMessage(message.jid, ldc, MessageType.text, { quoted: message.data })
         }
     }
