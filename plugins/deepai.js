6a8c10c3fe2cb476c77340508724a0a4896f4cce
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-09 13:59:37 +0530

    fail photo & photo

--- plugins/deepai.js
+++ plugins/deepai.js
@@ -336,36 +336,6 @@ if (Config.WORKTYPE == 'private') {
 
     }));
 
-    SlHackers.addCommand({ pattern: 'oldfoto ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
-
-        if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
-
-        var downloading = await message.client.sendMessage(message.jid, 'Creating.. ♻️', MessageType.text);
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-
-        ffmpeg(location)
-            .save('output.jpg')
-            .on('end', async () => {
-                var resp = await deepai.callStandardApi("CNNMRF", {
-                    content: fs.createReadStream("./output.jpg"),
-
-                });
-
-                var respoimage = await axios.get(`https://docs-jojo.herokuapp.com/api/colorize-old-photo?image_url=${resp.output_url}`, { responseType: 'arraybuffer' })
-
-                await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.BOT_NAAAME })
-
-            });
-
-        return await message.client.deleteMessage(message.jid, { id: downloading.key.id, remoteJid: message.jid, fromMe: true })
-
-    }));
 }
 else if (Config.WORKTYPE == 'public') {
 
--- plugins/oldphoto.js
+++ plugins/oldphoto.js
@@ -9,32 +9,11 @@ if (Config.WORKTYPE == 'private') {
 
   SlHackers.addCommand({ pattern: 'oldphoto ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
 
-    if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
+    if (match[1] === '') return await message.sendMessage(need);
 
-    var downloading = await message.client.sendMessage(message.jid, 'Creating.. ♻️', MessageType.text);
-    var location = await message.client.downloadAndSaveMediaMessage({
-      key: {
-        remoteJid: message.reply_message.jid,
-        id: message.reply_message.id
-      },
-      message: message.reply_message.data.quotedMessage
-    });
+    var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/colorize-old-photo?image_url=${match[1]}`, { responseType: 'arraybuffer' })
 
-    ffmpeg(location)
-      .save('output.jpg')
-      .on('end', async () => {
-        var resp = await deepai.callStandardApi("CNNMRF", {
-          content: fs.createReadStream("./output.jpg"),
-
-        });
-
-        var respoimage = await axios.get(`https://docs-jojo.herokuapp.com/api/colorize-old-photo?image_url=${resp.output_url}`, { responseType: 'arraybuffer' })
-
-        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.BOT_NAAAME })
-
-      });
-
-    return await message.client.deleteMessage(message.jid, { id: downloading.key.id, remoteJid: message.jid, fromMe: true })
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by :\nSLHackers Team' })
 
   }));
 }
