3da3efe41733616e1608a144178d932c9306d8d4
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-09 23:25:34 +0530

    Only happymod

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -11,11 +11,11 @@ if (Config.WORKTYPE == 'private') {
 
 	SlHackers.addCommand({ pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 		if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-		const url = `https://api.zeks.me/api/happymod?apikey=${Config.ASENA_PLUG}&q=${match[1]}`;
+		const url = `https://leyscoders-api.herokuapp.com/api/rexdl-search?q=${match[1]}&apikey=dappakntlll`;
 		try {
 			const response = await got(url);
 			const json = JSON.parse(response.body);
-			if (response.statusCode === 200) return await message.client.sendMessage(message.jid,
+			if (response.statusCode === true) return await message.client.sendMessage(message.jid,
 				'*📕 ' + Lang.NAME + '* ```' + json.result[0].title + '```\n\n' +
 				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].url + '```\n\n\n' +
 				'*📘 ' + Lang.RATING + '* ```' + json.result[0].rating + '```\n\n\n' +
@@ -29,11 +29,11 @@ else if (Config.WORKTYPE == 'public') {
 
 	SlHackers.addCommand({ pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 		if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-		const url = `https://api.zeks.me/api/happymod?apikey=${Config.ASENA_PLUG}&q=${match[1]}`;
+		const url = `https://leyscoders-api.herokuapp.com/api/rexdl-search?q=${match[1]}&apikey=dappakntlll`;
 		try {
 			const response = await got(url);
 			const json = JSON.parse(response.body);
-			if (response.statusCode === 200) return await message.client.sendMessage(message.jid,
+			if (response.statusCode === true) return await message.client.sendMessage(message.jid,
 				'*📕 ' + Lang.NAME + '* ```' + json.result[0].title + '```\n\n' +
 				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].url + '```\n\n\n' +
 				'*📘 ' + Lang.RATING + '* ```' + json.result[0].rating + '```\n\n\n' +
@@ -45,11 +45,11 @@ else if (Config.WORKTYPE == 'public') {
 
 	SlHackers.addCommand({ pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 		if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-		const url = `https://api.zeks.me/api/happymod?apikey=${Config.ASENA_PLUG}&q=${match[1]}`;
+		const url = `https://leyscoders-api.herokuapp.com/api/rexdl-search?q=${match[1]}&apikey=dappakntlll`;
 		try {
 			const response = await got(url);
 			const json = JSON.parse(response.body);
-			if (response.statusCode === 200) return await message.client.sendMessage(message.jid,
+			if (response.statusCode === true) return await message.client.sendMessage(message.jid,
 				'*📕 ' + Lang.NAME + '* ```' + json.result[0].title + '```\n\n' +
 				'*📗 ' + Lang.DOWNLOAD + ':* ```' + json.result[0].url + '```\n\n\n' +
 				'*📘 ' + Lang.RATING + '* ```' + json.result[0].rating + '```\n\n\n' +
