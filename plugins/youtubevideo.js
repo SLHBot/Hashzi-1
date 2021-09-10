5baa8d78bdf3fceced41a8ec7f44ddd591e408ef
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-10 17:02:19 +0530

    Only youtube updated

--- plugins/youtubevideo.js
+++ plugins/youtubevideo.js
@@ -15,6 +15,9 @@ const YT_NEED = "*need word!.*"
 const DWLOAD_VID = "*🎭Downloading Your Video...*"
 const YTV_UP = "*🚀Uploading Your Video...*"
 const NO_RESULT = "*🌀can't Find Anything...*"
+const PHN_PHN = "කාල සීමාව:-"
+const PHN_RDATE = "ශීර්ෂය:-"
+const PHN_CHAN = "අනුගමනය කරන්න-:"
 
 
 if (Config.WORKTYPE == 'private') {
@@ -30,12 +33,20 @@ if (Config.WORKTYPE == 'private') {
             .then(async (response) => {
                 const {
                     link,
+                    title,
+                    channel,
+                    duration,
+
                 } = response.data.result.link
 
                 const videoBuffer = await axios.get(link, { responseType: 'arraybuffer' })
 
+                const msg = `*${PHN_RDATE}* ${title}` + `\n` +
+                    `*${PHN_CHAN}* ${channel}` + `\n` +
+                    `*${PHN_PHN}* ${duration}`
+
                 await message.client.sendMessage(message.jid, YTV_UP, MessageType.text);
-                await message.client.sendMessage(message.jid, Buffer.from(videoBuffer.data), MessageType.video, { quoted: message.data, filename: 'SLHackers.mp4', mimetype: Mimetype.mp4, caption: Config.BOT_NAAAAAAAME, ptt: false })
+                await message.client.sendMessage(message.jid, Buffer.from(videoBuffer.data), MessageType.video, { quoted: message.data, filename: 'SLHackers.mp4', mimetype: Mimetype.mp4, caption: msg + '\n' + Config.BOT_NAAAAAAAME, ptt: false })
             })
             .catch(
                 async (err) => await message.client.sendMessage(message.jid, NO_RESULT, MessageType.text, { quoted: message.data }),
