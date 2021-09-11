9f6f860ee6a04c8fbdf38af6ba75b7f9c1f1a0e4
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-11 19:07:00 +0530

    carbon/😎/💳/spdf

--- plugins/slhautospdf.js
+++ plugins/slhautospdf.js
@@ -10,22 +10,7 @@ let Lang = Language.getString('amazone');
 
 if (Config.WORKTYPE == 'private') {
 
-    SlHackers.addCommand({ pattern: 'spdf ?(.*)', fromMe: true, desc: Lang.SPDF }, (async (message, match) => {
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.SPDF_LINK, MessageType.text, { quoted: message.data });
-
-        var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid, Lang.SPDF_PROC, MessageType.text, { quoted: message.data });
-
-        await message.sendMessage(Buffer.from(webimage.data), MessageType.document, { mimetype: Mimetype.pdf, filename: 'SLHackers-Team-Bot-Site-to-Pdf-Made-By-Hirusha Dayarathne.pdf', });
-
-    }));
-}
-
-else if (Config.WORKTYPE == 'public') {
-
-    SlHackers.addCommand({ pattern: 'spdf ?(.*)', fromMe: false, desc: Lang.SPDF }, (async (message, match) => {
+    SlHackers.addCommand({ pattern: '😎 ?(.*)', fromMe: true, desc: Lang.SPDF }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.SPDF_LINK, MessageType.text, { quoted: message.data });
 
---
+++ plugins/slhautoyt.js
@@ -0,0 +1,140 @@
+
+
+const SlHackers = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const got = require('got');
+const Config = require('../config');
+const Language = require('../language');
+
+const Lang = Language.getString('instagram');
+const Tlang = Language.getString('tiktok');
+const YTV_DESC = "යූටියුබ් වීඩියෝ බාගැනීම් V2 ."
+const YT_NEED = "*🔗 URL අවශ්‍යයි!.*"
+const DWLOAD_VID = "*🎭ඔබේ වීඩියෝ බාගැනීම...*"
+const YTV_UP = "*🚀ඔබේ වීඩියෝ පටය උඩුගත කිරීම...*"
+const NO_RESULT = "*🌀කිසිවක් සොයා ගත නොහැක...*"
+const PHN_DU = "⏰ කාල සීමාව:-"
+const PHN_RDATE = "🎚 ශීර්ෂය:-"
+const PHN_UP = "🛂 හිමිකරු :-"
+const PHN_VIEW = "👁‍🗨 බැලීම් :-"
+const PHN_LIKE = "👍🏻 කමැත්ත පලකළ ගණන:-"
+const PHN_DISLIKE = "👎🏻 අකමැත්ත පලකළ ගණන :-"
+const PHN_CHAN = "📺 හිමිකරුගේ තව video බැලීමට අනුගමනය කරන්න:-"
+const PHN_RES = "💾 Resolution :-"
+const PHN_HELPER = "*📢 Youtube Link Promoter :-* _හිරුවා_"
+
+
+if (Config.WORKTYPE == 'private') {
+
+    SlHackers.addCommand({ pattern: '💳 ?(.*)', fromMe: true, desc: YTV_DESC, deleteCommand: false }, async (message, match) => {
+
+        const link = match[1]
+
+        if (!link) return await message.client.sendMessage(message.jid, YT_NEED, MessageType.text)
+        await axios
+            .get(`https://api.lolhuman.xyz/api/ytvideo?apikey=${Config.LLHAPI}&url=${link}`)
+            .then(async (response) => {
+                const {
+                    link,
+                    resolution,
+
+                } = response.data.result.link
+
+                const {
+                    thumbnail,
+                    title,
+                    duration,
+                    uploader,
+                    channel,
+                    view,
+                    like,
+                    dislike,
+
+                } = response.data.result
+
+                const thumbnailBuffer = await axios.get(thumbnail, { responseType: 'arraybuffer' })
+
+                const slh = `*${PHN_RDATE}* ${title}` + `\n\n` +
+                    `*${PHN_CHAN}* ${channel}` + `\n\n` +
+                    `*${PHN_VIEW}* ${view}` + `\n\n` +
+                    `*${PHN_LIKE}* ${like}` + `\n\n` +
+                    `*${PHN_DISLIKE}* ${dislike}` + `\n\n` +
+                    `*${PHN_DU}* ${duration}` + `\n\n` +
+                    `${PHN_HELPER}`
+
+                await message.client.sendMessage(message.jid, Buffer.from(thumbnailBuffer.data), MessageType.image, { quoted: message.data, caption: slh });
+
+                const videoBuffer = await axios.get(link, { responseType: 'arraybuffer' })
+
+                const msg = `*${PHN_RDATE}* ${title}` + `\n\n` +
+                    `*${PHN_UP}* ${uploader}` + `\n\n` +
+                    `*${PHN_RES}* ${resolution}` + `\n\n` +
+                    `*${PHN_DU}* ${duration}` + `\n\n` +
+                    `${PHN_HELPER}`
+
+                await message.client.sendMessage(message.jid, Buffer.from(videoBuffer.data), MessageType.video, { quoted: message.data, filename: 'SLHackers.mp4', mimetype: Mimetype.mp4, caption: msg, ptt: false })
+            })
+            .catch(
+                async (err) => await message.client.sendMessage(message.jid, NO_RESULT, MessageType.text, { quoted: message.data }),
+            )
+    },
+    )
+}
+
+else if (Config.WORKTYPE == 'public') {
+    SlHackers.addCommand({ pattern: '💳 ?(.*)', fromMe: false, desc: YTV_DESC }, async (message, match) => {
+
+        const link = match[1]
+
+        if (!link) return await message.client.sendMessage(message.jid, YT_NEED, MessageType.text, { quoted: message.data })
+        await axios
+            .get(`https://api.lolhuman.xyz/api/ytvideo?apikey=${Config.LLHAPI}&url=${link}`)
+            .then(async (response) => {
+                const {
+                    link,
+                    resolution,
+
+                } = response.data.result.link
+
+                const {
+                    thumbnail,
+                    title,
+                    duration,
+                    uploader,
+                    channel,
+                    view,
+                    like,
+                    dislike,
+
+                } = response.data.result
+
+                const thumbnailBuffer = await axios.get(thumbnail, { responseType: 'arraybuffer' })
+
+                const slh = `*${PHN_RDATE}* ${title}` + `\n\n` +
+                    `*${PHN_CHAN}* ${channel}` + `\n\n` +
+                    `*${PHN_VIEW}* ${view}` + `\n\n` +
+                    `*${PHN_LIKE}* ${like}` + `\n\n` +
+                    `*${PHN_DISLIKE}* ${dislike}` + `\n\n` +
+                    `*${PHN_DU}* ${duration}` + `\n\n` +
+                    `${PHN_HELPER}`
+
+                await message.client.sendMessage(message.jid, Buffer.from(thumbnailBuffer.data), MessageType.image, { quoted: message.data, caption: slh });
+
+                const videoBuffer = await axios.get(link, { responseType: 'arraybuffer' })
+
+                const msg = `*${PHN_RDATE}* ${title}` + `\n\n` +
+                    `*${PHN_UP}* ${uploader}` + `\n\n` +
+                    `*${PHN_RES}* ${resolution}` + `\n\n` +
+                    `*${PHN_DU}* ${duration}` + `\n\n` +
+                    `${PHN_HELPER}`
+
+                await message.client.sendMessage(message.jid, Buffer.from(videoBuffer.data), MessageType.video, { quoted: message.data, filename: 'SLHackers.mp4', mimetype: Mimetype.mp4, caption: msg, ptt: false })
+            })
+            .catch(
+                async (err) => await message.client.sendMessage(message.jid, NO_RESULT, MessageType.text, { quoted: message.data }),
+            )
+    },
+    )
+}
