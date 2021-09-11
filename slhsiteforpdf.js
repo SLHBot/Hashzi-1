d5d3aaca89470ad778745c2bfd32ddbdf131f573
Hirusha21 <88909945+SLHBot@users.noreply.github.com>
2021-09-12 01:12:21 +0530

    ok

--- slhsiteforpdf.js
+++ slhsiteforpdf.js
@@ -1,44 +1,38 @@
-/* උස්සන්න එපා බේසිකෙ කොල්ලො අහලා ගනිම්
-Codded BY Hirusha Dayarathne
-Wa.me/+94755681782
-*/
-
-const SlHackers = require('../events');
-const Config = require('../config');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const axios = require('axios');
-const Language = require('../language');
-const SPDF_DESC = "Converts a Site into PDF"
-const SPDF_LINK = "*You Must Enter a URL*"
-const SPDF_PROC = "```Converting Site into PDF```"
-
+let SlHackers = require('../events');
+let { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+let fs = require('fs');
+let axios = require('axios');
+let request = require('request');
+let got = require("got");
+let Config = require('../config');
+let Language = require('../language');
+let Lang = Language.getString('unvoice');
 
 if (Config.WORKTYPE == 'private') {
 
-    SlHackers.addCommand({ pattern: 'spdf ?(.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
+    SlHackers.addCommand({ pattern: 'spdf ?(.*)', fromMe: true, desc: Lang.SPDF_DESC }, (async (message, match) => {
 
-        if (match[1] === '') return await message.sendMessage(SPDF_LINK);
+        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.SPDF_LINK, MessageType.text, { contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
         var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, SPDF_PROC, MessageType.text, { quoted: message.data });
+        await message.client.sendMessage(message.jid, Lang.SPDF_PROC, MessageType.text, { contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
-        await message.client.sendMessage(message.jid, Buffer.from(webimage.data), MessageType.document, { filename: 'SLHackers.pdf', mimetype: Mimetype.pdf })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.document, { mimetype: Mimetype.pdf, contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data, filename: 'SlHackers-Site-To-PDF-Feature-Made-By-Hirusha-Dayarathne.pdf' })
 
     }));
 }
 else if (Config.WORKTYPE == 'public') {
 
-    SlHackers.addCommand({ pattern: 'spdf ?(.*)', fromMe: false, dontAddCommandList: true }, (async (message, match) => {
+    SlHackers.addCommand({ pattern: 'spdf ?(.*)', fromMe: false, desc: Lang.SPDF_DESC }, (async (message, match) => {
 
-        if (match[1] === '') return await message.sendMessage(SPDF_LINK);
+        if (match[1] === '') return await message.sendMessage(message.jid, Lang.SPDF_LINK, MessageType.text, { contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
         var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, SPDF_PROC, MessageType.text, { quoted: message.data });
+        await message.sendMessage(message.jid, Lang.SPDF_PROC, MessageType.text, { contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
-        await message.client.sendMessage(message.jid, Buffer.from(webimage.data), MessageType.document, { filename: 'SLHackers.pdf', mimetype: Mimetype.pdf })
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.document, { mimetype: Mimetype.pdf, contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data, filename: 'SlHackers-Site-To-PDF-Feature-Made-By-Hirusha-Dayarathne.pdf' })
 
     }));
 }
